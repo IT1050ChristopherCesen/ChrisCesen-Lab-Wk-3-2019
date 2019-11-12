afb107fa87952e61eccfcef27ce9939f44509b04
IT1050ChristopherCesen <43357952+IT1050ChristopherCesen@users.noreply.github.com>
2019-11-11 20:31:20 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,93 @@
 ﻿using System;
 
-namespace ChrisCesenLabWk3_11102019
+namespace ChrisCesen_Wk3Lab_11102019
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /* Question 1:  Both execute their purpose if a certaitn condition is met.  
+             * The single selection statement does this if the condition is a single line or example.   
+             * The while statement will perform a function many times is the criteria is met.
+             * */
+
+            /*
+            int speedLimit =35;
+            int speed =42;
+
+            if (speed > speedLimit)
+            {
+                Console.WriteLine("Slow Down");
+            }
+            */
+
+            /*
+            bool isTrue;
+            isTrue = true;
+           
+            if (isTrue)
+            {
+                Console.WriteLine("It is True");
+            }
+            else
+            {
+                Console.WriteLine("It Is False");
+            }
+            */
+
+            /*
+            double fahrenheight = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheight - 32d) * 5d / 9d;
+            
+            Console.WriteLine("Enter Value");
+            Console.WriteLine(celsius);
+
+            if(fahrenheight < 40)
+            {
+                Console.WriteLine("It is Cold");
+            }
+
+            if (fahrenheight > 90)
+            {
+                Console.WriteLine("It is Hot");
+            }
+            */
+
+            /*
+            int counter = 1;
+            while (counter <= 10)
+
+            {
+                Console.WriteLine(counter);
+                counter = counter+1 ;
+            }
+            */
+
+            /*
+                       int counter = 60;
+                       while (counter >= 20)
+
+                       {
+                           Console.WriteLine(counter);
+                           counter = counter -1;
+                       }
+                       */
+
+            /*
+            int counter = 10;
+            while (counter <= 20)
+
+            {
+                Console.WriteLine(counter);
+                counter = counter + 1;
+            }
+            */
+
+
+
+
+
         }
     }
 }
